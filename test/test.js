b78e751f54f170181bea7db46d55e6d8d337530d
Mark Oosterveld <marko@oanda.com>
2011-10-24 16:20:00 -0400

    Notify our users if we lose connection

--- test/test.js
+++ test/test.js
@@ -2,10 +2,15 @@ var spread = require('spread');
 
 var toolkit = spread.SpreadToolkit(
     {port: 9333,
-     host: "oanda-app-dev",
+//     host: "oanda-app-dev",
+     host: "localhost",
      user: "moz",
      groups: ["a", "b"]
     });
+
+toolkit.on('close', function(had_error) {
+    console.log("closed!");
+});
 
 toolkit.start(function(flag) {
     console.log("flag: " + flag);
