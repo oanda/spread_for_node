57943056e04776da60b56be69fa0521ad51a0603
Mark Oosterveld <marko@oanda.com>
2011-10-25 08:50:24 -0400

    fixed the disconnect/reconnect code

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var spread = require('spread');
 
-var toolkit = spread.SpreadToolkit(
-    {port: 9333,
+var toolkit = spread.SpreadToolkit( {
+     port: 9333,
 //     host: "oanda-app-dev",
      host: "localhost",
      user: "moz",
@@ -10,7 +10,20 @@ var toolkit = spread.SpreadToolkit(
 
 toolkit.on('close', function(had_error) {
     console.log("closed!");
-});
+
+   var fn = function(success) {
+        if(!success) {
+            setTimeout(function() {
+                console.log('starting');
+                toolkit.start(fn)
+            }.bind(this), 30000);   // 30 seconds
+        } else {
+            console.log('SpreadToolkit re-connected. Resetting Maintenance Mode');
+            delete fn;
+        }
+    }.bind(this);
+    fn();
+}.bind(this));
 
 toolkit.start(function(flag) {
     console.log("flag: " + flag);
